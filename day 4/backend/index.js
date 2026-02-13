2239b15f1d568211b420c7fa547a62632477feb5
Aditya Shivale <Shivaleaditya22@gmail.com>
2026-02-13 22:24:29 +0530

    day 4 Added GET & POST methods in form

---
+++ day 4/backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 8080;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+
+app.get('/register', (req ,res) =>{
+    let {username , password} =req.query;
+    res.send(`Standard GET request. Welconme ${username}`)
+})
+
+app.post('/register', (req , res) =>{
+    let {username , password} = req.body;
+    res.send(`Standard POST request. Welcome ${username} `)
+})
+
+app.listen(port , () =>{
+    console.log(`App is listening on ${port}`)
+})
