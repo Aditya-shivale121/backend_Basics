552ebca2ac8121a58290a3832599030a3ac44ece
Aditya Shivale <Shivaleaditya22@gmail.com>
2026-02-14 22:59:22 +0530

    Add day5

---
+++ day5/REST_APIS/index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const port = 8080;
+
+const { v4 : uuidv4 }=require('uuid');
+
+
+
+app.use(express.urlencoded({ extended:true}));
+
+app.set("view engine" , "ejs");
+app.set("views", path.join(__dirname , "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        id : uuidv4(),
+        username : "Adi",
+        content : "Systems are the keys to wealth",
+    },
+    {
+        id : uuidv4(),
+        username : "Sid ",
+        content : "I love traveling",
+    },
+    {
+        id : uuidv4(),
+        username : "sam  ",
+        content : "Hardwork beats smartness..!",
+    }
+];
+
+app.get('/posts', (req ,res) =>{
+    res.render("index.ejs" , {posts})
+})
+
+app.get('/posts/new' ,(req ,res) =>{
+    res.render("new.ejs")
+})
+
+app.post('/posts', (req ,res) =>{
+    let {username , content} = req.body;
+    posts.push({
+        id :uuidv4(),
+        username :username ,
+         content : content});
+    res.redirect("/posts")
+})
+
+
+
+app.get('/posts/:id', (req ,res) =>{
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render('show.ejs',{post})
+    })
+
+
+app.patch('/posts/:id' ,(req ,res) =>{
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.send('pat reqchuest working');
+})
+
+
+app.listen(port , () =>{
+    console.log(`app is listening on ${port}`)
+})
